b1cac70ed9f0da959a9891cfa2fbe572d21b8a59
Aminnn <45661298+aminnoorani@users.noreply.github.com>
2023-03-21 15:49:37 -0400

    Update 02_input_Preparation.sh

--- 02_input_Preparation.sh
+++ 02_input_Preparation.sh
@@ -5,3 +5,9 @@ java -mx90G -jar ~/ChromHMM/ChromHMM.jar MakeSegmentation -printstatebyline ~/mo
 ####metadata files is a txt file that has to have one culumn which is id
 ####preprossess.sh file is downloaded from epilogos page
 bash preprocess_data_ChromHMM.sh /path/to/ChromHMM/STATEBYLINE /path/to/metadata_Boix.txt /path/to/hg38.genome
+
+
+
+#####
+epilogos -l -i PATH/TO/preprocessed_outputs -j PATH/TO/STATE_INFO_TSV -o PATH/TO/OUTPUT_DIR
+
